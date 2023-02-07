d32e21c2d777cde758d26b135dc09f6ed3697053
Brandon Jacobs <jacobs27@byu.edu>
2023-02-06 19:54:06 -0700

    Mission 5

---
+++ Mission4_jacobs27/Controllers/BrandonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission4_jacobs27.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_jacobs27.Controllers
+{
+    public class BrandonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult gradeCalculator() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult gradeCalculator(gradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
